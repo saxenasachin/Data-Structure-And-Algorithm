3eb874a1e210b07b1e3741fbdf6d4021a56425e4
Sachin Saxena <sachin.saxena1790@gmail.com>
2022-06-22 01:50:36 +0530

    bug fix - NullPointerException

--- src/advanceddsa/hashing1/DistinctNumbersInWindow.java
+++ src/advanceddsa/hashing1/DistinctNumbersInWindow.java
@@ -1,9 +1,6 @@
 package advanceddsa.hashing1;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Problem Description
@@ -72,7 +69,7 @@ public class DistinctNumbersInWindow {
         // iterate remaining elements one by one, remove an element from start
         // and add one element at end and calculate unique count of elements.
         for (int i = B; i < A.size(); i++) {
-            window.remove(i - B);
+            window.remove(0);
             window.add(A.get(i));
             output.add(countOfUniqueNumbers(window));
         }
@@ -80,7 +77,6 @@ public class DistinctNumbersInWindow {
     }
 
     /**
-     *
      * @param A - input list
      * @return - no of unique elements
      */
@@ -89,13 +85,65 @@ public class DistinctNumbersInWindow {
         return set.size();
     }
 
+    /**
+     * Create an empty hash map. Let the hash map be hM.
+     * Initialize the count of distinct elements as dist_count to 0.
+     * Traverse through the first window and insert elements of the first window to hM. The elements are used as key and their counts as the value in hM. Also, keep updating dist_count
+     * Print distinct count for the first window.
+     * Traverse through the remaining array (or other windows).
+     * Remove the first element of the previous window.
+     * If the removed element appeared only once, remove it from hM and decrease the distinct count, i.e. do “dist_count–“
+     * else (appeared multiple times in hM), then decrement its count in hM
+     * Add the current element (last element of the new window)
+     * If the added element is not present in hM, add it to hM and increase the distinct count, i.e. do “dist_count++”
+     * Else (the added element appeared multiple times), increment its count in hM
+     */
+    public static ArrayList<Integer> getDistinctCount(ArrayList<Integer> A, int B) {
+        ArrayList<Integer> output = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        int distinctCount = 0;
+        for (int i = 0; i < B; i++) { // Store all elements in window
+            int num = A.get(i);
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(A.get(i), 1);
+                distinctCount++;
+            }
+        }
+        output.add(distinctCount);
+
+        // iterate remaining elements one by one, remove an element from start
+        // and add one element at end and calculate unique count of elements.
+        for (int i = B; i < A.size(); i++) {
+            int numToRemove = A.get(i - B);
+            if (map.containsKey(numToRemove)) {
+                if (map.get(numToRemove) > 1) {
+                    map.put(numToRemove, map.get(numToRemove) - 1);
+                } else {
+                    map.remove(numToRemove);
+                    distinctCount--;
+                }
+            }
+            int numToAdd = A.get(i);
+            if (map.containsKey(numToAdd)) {
+                map.put(numToAdd, map.get(numToAdd) + 1);
+            } else {
+                map.put(numToAdd, 1);
+                distinctCount++;
+            }
+            output.add(distinctCount);
+        }
+        return output;
+    }
+
     public static void main(String[] args) {
-        int[] A = {1, 2, 1, 3, 4, 3};
+        int[] A = {31, 51, 31, 51, 31, 31, 31, 31, 31, 31, 51, 31, 31};
         int B = 3;
         ArrayList<Integer> input = new ArrayList<>();
         for (int a : A) {
             input.add(a);
         }
-        System.out.println(dNums(input, B));
+        System.out.println(getDistinctCount(input, B));
     }
 }
