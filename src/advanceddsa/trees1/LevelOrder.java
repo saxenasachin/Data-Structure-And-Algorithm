0f528d304cf92857d4783ba19b4d702540506b66
Sachin Saxena <sachin.saxena1790@gmail.com>
2022-10-09 14:44:35 +0530

    resolve merge conflicts

---
+++ src/advanceddsa/trees1/LevelOrder.java
@@ -0,0 +1,65 @@
+package advanceddsa.trees1;
+
+/**
+ * Problem Description
+ * Given a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+ * <p>
+ * <p>
+ * <p>
+ * Problem Constraints
+ * 1 <= number of nodes <= 105
+ * <p>
+ * <p>
+ * <p>
+ * Input Format
+ * First and only argument is root node of the binary tree, A.
+ * <p>
+ * <p>
+ * <p>
+ * Output Format
+ * Return a 2D integer array denoting the zigzag level order traversal of the given binary tree.
+ * <p>
+ * <p>
+ * <p>
+ * Example Input
+ * Input 1:
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * Input 2:
+ * <p>
+ * 1
+ * / \
+ * 6   2
+ * /
+ * 3
+ * <p>
+ * <p>
+ * Example Output
+ * Output 1:
+ * <p>
+ * [
+ * [3],
+ * [9, 20],
+ * [15, 7]
+ * ]
+ * Output 2:
+ * <p>
+ * [
+ * [1]
+ * [6, 2]
+ * [3]
+ * ]
+ * <p>
+ * <p>
+ * Example Explanation
+ * <p>
+ * Explanation 1:
+ * <p>
+ * Return the 2D array. Each row denotes the traversal of each level.
+ */
+public class LevelOrder {
+}
